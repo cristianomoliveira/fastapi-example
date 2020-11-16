912ec16951aeaa605070c6cfc26ee36648ed316a
Cristiano Oliveira <cristianomoliveira@gmail.com>
2020-11-16 09:58:11 -0300

    config CORS

--- app/main.py
+++ app/main.py
@@ -11,15 +11,6 @@ from starlette.responses import JSONResponse, Response, UJSONResponse
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=['*'])
 
-# Set all CORS enabled origins
-# if settings.BACKEND_CORS_ORIGINS:
-#     app.add_middleware(
-#         CORSMiddleware,
-#         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-#         allow_credentials=True,
-#         allow_methods=["*"],
-#         allow_headers=["*"],
-#     )
 
 
 class User(BaseModel):
